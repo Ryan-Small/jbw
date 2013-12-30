e0e73f455a8243a62087c0aeaba9b047b9df5a82
Ryan Small <Ryan-Small@Harbinger-Technologies.com>
2013-12-30 11:32:16 -0500

    updated gitignore to remove unnecessary files

--- release/run.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-REM change back to the current directory (if run as administrator)
-cd %~dp0
-echo You may need to specify a 32-bit installation of java if you get an UnsatisfiedLinkError
-REM start the java bot
-java -jar jnibwapi.jar
-pause
